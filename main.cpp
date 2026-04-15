6271706e940c45f81cdb248d284ed012dbadd576
ElektrischeErbse <1846349950@qq.com>
2026-04-16 01:15:05 +0800

    refactor(project): restructure project layout and add test infrastructure
    Create tests/ directory for unit test files
    Move test_requests.cpp into tests/
    Rewrite test code using GoogleTest framework
    Add README.md for project documentation
    Update project structure and CMake configuration accordingly

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by tom on 26-4-15.
+//
+
+#include <iostream>
+#include <requests/requests.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+    return 0;
+}
---
+++ tests/test_requests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by tom on 26-4-14.
+//
+
+#include <requests/requests.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
